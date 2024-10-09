d971839208a8cf2367f60f3c3c70dc27d44ac7c7
Omoakinni <ariyotomiwa@gmail.com>
2024-10-09 23:10:08 +0100

    Complete Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Hello Prep 1 World!");
         Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
+        string fname = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        string lname = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}"); 
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}."); 
     }
 }
